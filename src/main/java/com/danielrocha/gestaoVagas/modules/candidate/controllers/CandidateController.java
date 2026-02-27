141297545c0494bd9d86f62cf71d60b2296eb26d
daniel9189 <dbrito4321@gmail.com>
2026-02-27 15:57:51 -0300

    feat: add candidate field validation

--- src/main/java/com/danielrocha/gestaoVagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/danielrocha/gestaoVagas/modules/candidate/controllers/CandidateController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.danielrocha.gestaoVagas.modules.candidate.CandidateEntity;
 
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class CandidateController {
     
     @PostMapping("/")
-    public void create(@RequestBody CandidateEntity candidateEntity) {
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
     System.out.println("Candidato");
     System.out.println(candidateEntity.getEmail());
     }
